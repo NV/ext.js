f425801f22c3ca17bd23c8b527f3e00e4a3e2f4c
visionmedia <tj@vision-media.ca>
2009-12-30 10:18:28 -0800

    Specs for String#capitalize()

--- spec/string.spec.js
+++ spec/string.spec.js
@@ -11,4 +11,19 @@ describe 'String'
       'FOO'.lowercase.should.eql 'foo'
     end
   end
+  
+  describe '#capitalize()'
+    describe 'given no arguments'
+      it 'should capitalize the first word'
+        'foo bar'.capitalize().should.eql 'Foo bar'
+      end
+    end
+    
+    describe 'given true'
+      it 'should capitalize all words'
+        'foo bar baz'.capitalize(true).should.eql 'Foo Bar Baz'
+        'foo bar baz'.capitalize('all').should.eql 'Foo Bar Baz'
+      end
+    end
+  end
 end
