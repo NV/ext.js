6ab142cbe3bcaeaca2bd48f8d3aeaaae29921ce3
visionmedia <tj@vision-media.ca>
2010-01-01 08:27:30 -0800

    Added Date#dayName

--- spec/date.spec.js
+++ spec/date.spec.js
@@ -1,8 +1,14 @@
 
 describe 'Date'
   describe '#monthName'
-    it 'should return the month name string'
+    it 'should return a month name'
       (new Date('May 5 1987')).monthName.should.eql 'May'
+    end
+  end
+  
+  describe '#dayName'
+    it 'should return a day names'
+      (new Date('May 5 1987')).dayName.should.eql 'Tuesday'
     end
   end
 end
