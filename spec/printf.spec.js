d79b354bd0b7031cc643cf43775af75bd243d2c3
visionmedia <tj@vision-media.ca>
2010-01-01 12:35:41 -0800

    Started implementing sprintf()

--- spec/printf.spec.js
+++ spec/printf.spec.js
@@ -1,6 +1,11 @@
 
-printf = require('ext/printf').printf
+sprintf = require('ext/printf').sprintf
 
-describe 'printf()'
-
+describe 'sprintf()'
+  describe 'c'
+    it 'should substitute a character'
+      sprintf('%c', 'a').should.eql 'a'
+      sprintf('%c', 'abc').should.eql 'a'
+    end
+  end
 end
