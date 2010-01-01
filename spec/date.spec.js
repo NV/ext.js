789e596ca8ad5d50c891d5bdb47931b07be69a3f
visionmedia <tj@vision-media.ca>
2010-01-01 08:31:59 -0800

    Added short variations of monthName and dayName

--- spec/date.spec.js
+++ spec/date.spec.js
@@ -6,9 +6,21 @@ describe 'Date'
     end
   end
   
+  describe '#shortMonthName'
+    it 'should return a short month name'
+      (new Date('January 5 1987')).shortMonthName.should.eql 'Jan'
+    end
+  end
+  
   describe '#dayName'
-    it 'should return a day names'
+    it 'should return a day name'
       (new Date('May 5 1987')).dayName.should.eql 'Tuesday'
+    end
+  end
+  
+  describe '#shortDayName'
+    it 'should return a short day name'
+      (new Date('May 5 1987')).shortDayName.should.eql 'Tue'
     end
   end
 end
